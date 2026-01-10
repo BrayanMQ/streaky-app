3432dca1c6748df62fffb2ce5a04a7eda1c48a73
Brayan Steven Marín Quirós <49928451+BrayanMQ@users.noreply.github.com>
2026-01-10 12:26:30 -0600

    feat: add i18next-browser-languagedetector for improved language detection
    - Integrated i18next-browser-languagedetector to enhance language detection capabilities.
    - Updated i18n configuration to utilize localStorage and navigator for determining user language preferences.

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,19 +1,26 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './locales/en.json'
 import es from './locales/es.json'
 
-i18n.use(initReactI18next).init({
-    resources: {
-        en: { translation: en },
-        es: { translation: es },
-    },
-    lng: 'en',
-    fallbackLng: 'en',
-    interpolation: {
-        escapeValue: false,
-    },
-})
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: { translation: en },
+            es: { translation: es },
+        },
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+        detection: {
+            order: ['localStorage', 'navigator'],
+            caches: ['localStorage'],
+        },
+    })
 
 export default i18n
